7ee47a40a1ed7278d024bd58b66cb8f871081cb0
Gwyn Pritchett <gwyn@gwyn.ca>
2017-10-27 11:16:11 +0300

    intial commit

---
+++ wp-content/themes/spiralgroup/page.php
@@ -0,0 +1,9 @@
+<?php get_template_part('templates/page', 'header'); ?>
+
+<section class="content">
+  <div class="container">
+    <?php while (have_posts()) : the_post(); ?>
+      <?php get_template_part('templates/content', 'page'); ?>
+    <?php endwhile; ?>
+  </div>
+</section>
---
+++ wp-content/themes/spiralgroup/templates/footer.php
@@ -0,0 +1,5 @@
+<footer class="section content-info">
+  <div class="container">
+    <?php dynamic_sidebar('sidebar-footer'); ?>
+  </div>
+</footer>
