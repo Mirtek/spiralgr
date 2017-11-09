b586a0584a8f75a396cf2a19ad685eccd8f5315c
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 15:36:14 +0200

    Clear commented code

--- wp-content/themes/spiralgroup/templates/footer.php
+++ wp-content/themes/spiralgroup/templates/footer.php
@@ -1,46 +1,41 @@
-<!--<footer class="section content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>-->
 <footer>
-    <div class="content-wrapper">
-      <div class="logo-container">
-        <a href="<?= esc_url(home_url('/')); ?>"></a>
+  <div class="content-wrapper">
+    <div class="logo-container">
+      <a href="<?= esc_url(home_url('/')); ?>"></a>
+    </div>
+    <div class="contact-us-container">
+      <div class="title">
+        Contact Us
       </div>
-      <div class="contact-us-container">
-        <div class="title">
-          Contact Us
-        </div>
-        <div class="address-container">
-          <p>350 Townsend Street</p>
-          <p>Suite 777</p>
-          <p>San Francisco, CA 94107</p>
-          <p>415-869-8579</p>
-          <p class="email"><a href="mailto:info@spiralgroup.com">info@spiralgroup.com</a></p>
-        </div>
+      <div class="address-container">
+        <p>350 Townsend Street</p>
+        <p>Suite 777</p>
+        <p>San Francisco, CA 94107</p>
+        <p>415-869-8579</p>
+        <p class="email"><a href="mailto:info@spiralgroup.com">info@spiralgroup.com</a></p>
       </div>
-      <div class="footer-menu-container">
-        <?php wp_nav_menu( array('menu' => 'footer_menu' )); ?>
-      </div>
-      <div class="footer-social-links-container">
-        <ul class="social-links">
-          <li class="social-links-item">
-            <a href="">
-              Twitter 
-            </a>
-          </li>
-          <li class="social-links-item">
-            <a href="">
-              Facebook
-            </a>
-          </li>
-          <li class="social-links-item">
-            <a href="">
-              LinkedIn
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>    
-  </footer>
+    </div>
+    <div class="footer-menu-container">
+      <?php wp_nav_menu( array('menu' => 'footer_menu' )); ?>
+    </div>
+    <div class="footer-social-links-container">
+      <ul class="social-links">
+        <li class="social-links-item">
+          <a href="">
+            Twitter 
+          </a>
+        </li>
+        <li class="social-links-item">
+          <a href="">
+            Facebook
+          </a>
+        </li>
+        <li class="social-links-item">
+          <a href="">
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>    
+</footer>
--- wp-content/themes/spiralgroup/templates/header.php
+++ wp-content/themes/spiralgroup/templates/header.php
@@ -1,49 +1,23 @@
 <?php use Roots\Sage\Assets; ?>
-<!--<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>-->
+
 <header>
-    <div class="content-wrapper">
-      <div class="logo-container">
-        <div class="logo">
-          <a href="<?= esc_url(home_url('/')); ?>"></a>
-        </div>
-        <div class="slogan">
-          Brand Marketing For Emerging Technology Companies
-        </div>
+  <div class="content-wrapper">
+    <div class="logo-container">
+      <div class="logo">
+        <a href="<?= esc_url(home_url('/')); ?>"></a>
       </div>
-      <div class="menu-button">
-        <i class="ico fa fa-bars" aria-hidden="true"></i>
+      <div class="slogan">
+        Brand Marketing For Emerging Technology Companies
       </div>
     </div>
-    <div class="menu-block">
-      <div class="menu-block-close-btn">
-        <i class="fa fa-times" aria-hidden="true"></i>
-      </div>      
-      <?php wp_nav_menu(); ?>
-      <!-- <li class="menu-item">
-        <a href="">Services</a>
-      </li>
-      <li class="menu-item">
-        <a href="">Results</a>
-      </li>
-      <li class="menu-item">
-        <a href="">About Us</a>
-      </li>
-      <li class="menu-item">
-        <a href="">Insights</a>
-      </li>
-      <li class="menu-item">
-        <a href="">Contact Us</a>
-      </li> -->
-    </div> 
-  </header>
+    <div class="menu-button">
+      <i class="ico fa fa-bars" aria-hidden="true"></i>
+    </div>
+  </div>
+  <div class="menu-block">
+    <div class="menu-block-close-btn">
+      <i class="fa fa-times" aria-hidden="true"></i>
+    </div>      
+    <?php wp_nav_menu(); ?>  
+  </div> 
+</header>
