b6d096c49a695c3528f0ae05ffd25627c194c53d
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 17:35:45 +0200

    Style post list

--- wp-content/themes/spiralgroup/templates/content.php
+++ wp-content/themes/spiralgroup/templates/content.php
@@ -3,7 +3,7 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
-  <div class="entry-summary">
+  <div class="entry-summary entry-summary-padding">
     <?php the_excerpt(); ?>
   </div>
 </article>
