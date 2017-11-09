fc67aeea5cffd5a07217021a783a90ba2c373a30
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 15:38:41 +0200

    use page-header template

--- wp-content/themes/spiralgroup/page.php
+++ wp-content/themes/spiralgroup/page.php
@@ -1,13 +1,5 @@
-<?php /*get_template_part('templates/page', 'header');*/ ?>
-
-<section class="first-section" style="background: url(<?php echo(get_the_post_thumbnail_url( $post,'post-thumbnail' )); ?>); background-repeat: no-repeat;background-position: 50%; background-size: 100% 100%; height: 61vw; position: relative;">
-    <h1 class="content col-xs-10">
-      <div class="main-title">
-        <?php the_title(); ?>
-      </div>
-    </h1>
-    <?php get_template_part('templates/entry-meta'); ?>
-    </section> 
+<?php get_template_part('templates/page', 'header'); ?>
+ 
 <section class="content">
   <div class="container">
     <?php while (have_posts()) : the_post(); ?>
