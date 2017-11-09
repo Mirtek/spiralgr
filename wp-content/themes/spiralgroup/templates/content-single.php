bda1c4b666ce1d6ad424af31ee1bd5ac9f3b9df3
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 15:50:00 +0200

    Show post-thumbnail or default image in header from page-header

--- wp-content/themes/spiralgroup/templates/content-single.php
+++ wp-content/themes/spiralgroup/templates/content-single.php
@@ -1,7 +1,7 @@
 <?php use Roots\Sage\Extras; ?>
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <?php get_template_part('templates/page', 'header'); ?>
+ <?php get_template_part('templates/page', 'header'); ?>
     <div class="size-img"></div>
     <section class="article-section">
     <div class="content-wrapper">
--- wp-content/themes/spiralgroup/templates/page-header.php
+++ wp-content/themes/spiralgroup/templates/page-header.php
@@ -1,7 +1,11 @@
 <?php use Roots\Sage\Titles; ?>
 
 
-<section class="first-section" style="background: url(<?php echo(get_the_post_thumbnail_url( $post->ID,'post-thumbnail' )); ?>); background-repeat: no-repeat;background-position: 50%; background-size: 100% 100%; height: 61vw; position: relative;">
+<?php if (has_post_thumbnail()) {?>
+  <section class="first-section" style="background: url(<?php echo(get_the_post_thumbnail_url( $post,'post-thumbnail' )); ?>); background-repeat: no-repeat;background-position: 50%; background-size: 100% 100%; height: 61vw; position: relative;">
+<?php } else {?>
+  <section class="first-section">
+<?php } ?>
     <h1 class="content col-xs-10">
       <div class="main-title">
         <?php the_title(); ?>
