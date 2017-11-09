4cfc8c1d3e40378b521108a3734f334aa16a4a14
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 13:15:24 +0200

    Create header style for 404 and page

--- wp-content/themes/spiralgroup/404.php
+++ wp-content/themes/spiralgroup/404.php
@@ -1,7 +1,13 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<!-- <?php get_template_part('templates/page', 'header'); ?> -->
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
+<section class="first-section" style="background: url(<?php echo(get_the_post_thumbnail_url( $post,'post-thumbnail' )); ?>); background-repeat: no-repeat;background-position: 50%; background-size: 100% 100%; height: 61vw; position: relative;">
+    <h1 class="content col-xs-10">
+      <div class="main-title">
+        <?php _e('Error 404. Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+      </div>
+    </h1>
+    <?php get_template_part('templates/entry-meta'); ?>
+    </section> 
+<section class="content">
 
 <?php get_search_form(); ?>
