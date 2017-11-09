7b6cd36c39e6cdc255f56013d073f903d8f78d94
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 17:05:16 +0200

    Change blog posts list view

--- wp-content/themes/spiralgroup/index.php
+++ wp-content/themes/spiralgroup/index.php
@@ -1,4 +1,4 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<!-- <?php get_template_part('templates/page', 'header'); ?> -->
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
@@ -7,11 +7,14 @@
   <?php get_search_form(); ?>
 
 <?php endif; ?>
-
+ <div class="container">
 <?php while (have_posts()) : the_post(); ?>
+<?php if (has_post_thumbnail()) {
+	the_post_thumbnail('hero-thumb', array('class' => 'img-fluid'));
+}?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
-
+</div>
 <?php the_posts_navigation(); ?>
 
 
