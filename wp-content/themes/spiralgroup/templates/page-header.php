e290138afe1fa704ea19984a9ec8ad39c605c2e3
Denys Toloshny <gg.zyus@gmail.com>
2017-11-09 15:38:20 +0200

    Move header with title to page-header template

--- wp-content/themes/spiralgroup/templates/page-header.php
+++ wp-content/themes/spiralgroup/templates/page-header.php
@@ -1,5 +1,11 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
-</div>
+
+<section class="first-section" style="background: url(<?php echo(get_the_post_thumbnail_url( $post->ID,'post-thumbnail' )); ?>); background-repeat: no-repeat;background-position: 50%; background-size: 100% 100%; height: 61vw; position: relative;">
+    <h1 class="content col-xs-10">
+      <div class="main-title">
+        <?php the_title(); ?>
+      </div>
+    </h1>
+    <?php get_template_part('templates/entry-meta'); ?>
+</section>
