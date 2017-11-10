2d8036bbddbb4d9f4e58b359453fa97b3e408fad
Gwyn Pritchett <gwyn@gwyn.ca>
2017-11-10 09:08:42 +0000

    add wrappers for page.php

--- wp-content/themes/spiralgroup/templates/content-page.php
+++ wp-content/themes/spiralgroup/templates/content-page.php
@@ -1,2 +1,11 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php 
+  $wrapping = get_field('page_wrapping');
+?>
+<div class="row">
+  <div class="<?= $wrapping ? 'wrapper-' . $wrapping : 'wrapper-full'; ?>">
+    <div class="entry-content">
+      <?php the_content(); ?>
+      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </div>
+  </div>
+</div>
