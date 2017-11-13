6e4c1eb3f983ca3e527e91c23f263dd9263fbc3e
Denys Toloshny <gg.zyus@gmail.com>
2017-11-13 12:01:07 +0200

    add testimonials section template (no logo yet)

--- wp-content/themes/spiralgroup/templates/section-testimonials.php
+++ wp-content/themes/spiralgroup/templates/section-testimonials.php
@@ -1,1 +1,32 @@
-Testimonials
+<section class="testimonials-section">
+    <div class="content-wrapper">
+        <div class="custom-container">
+            <div class="title">
+                Testimonials
+            </div>
+            <div class="swiper-slider">
+                <div class="swiper-wrapper">
+                    <?php $testimonials = get_sub_field('testimonials'); ?>
+                    <?php if ( $testimonials ): ?> 
+                        <?php foreach ( $testimonials as $tes ): ?>
+                            <div class="swiper-slide">
+                                <div class="content">
+                                    <?php echo ($tes['testimonial']); ?>
+                                </div>  
+                                <div class="author-container">
+                                    <div class="author">
+                                        <?php echo ($tes['name']); ?>
+                                    </div>
+                                    <div class="position">
+                                        <?php echo ($tes['company']); ?>
+                                    </div>
+                                    <div class="logo"></div>
+                                </div>
+                            </div><!-- swiper-slide end -->
+                        <?php endforeach; ?>
+                    <?php endif; ?> 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
