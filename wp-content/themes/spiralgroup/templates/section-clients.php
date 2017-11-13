90a7662f821004b34e3e0c290cffda35770067fe
Ievgenii Bondarenko <jenik_bond@ukr.net>
2017-11-13 16:41:55 +0200

    add section-clients template (no logo)

--- wp-content/themes/spiralgroup/templates/section-clients.php
+++ wp-content/themes/spiralgroup/templates/section-clients.php
@@ -1,1 +1,39 @@
-Clients
+<section class="select-clients-section">
+    <div class="content-wrapper">
+      <div class="title">
+        <?php the_sub_field('title'); ?>
+      </div>
+      <div class="clients-list">
+         <?php if ( have_rows('clients') ) : 
+                while ( have_rows('clients') ) : the_row(); 
+            ?>
+        <div class="clients-item">
+          <div class="company-logo company-1">
+            
+          </div>
+          <div class="hover-description">
+            <p class="value">
+              <?php the_sub_field('number_1'); ?>
+            </p>
+            <p class="desc">
+              <?php the_sub_field('number_1_description'); ?>
+            </p>
+            <p class="value">
+              <?php the_sub_field('number_2'); ?>
+            </p>
+            <p class="desc">
+              <?php the_sub_field('number_2_description'); ?>
+            </p>
+            <p class="value">
+              <?php the_sub_field('number_3'); ?>
+            </p>
+            <p class="desc">
+              <?php the_sub_field('number_3_description'); ?>
+            </p>
+          </div>
+        </div>
+         <?php endwhile; ?>
+                <?php endif; ?> 
+      </div>
+    </div>
+  </section>
