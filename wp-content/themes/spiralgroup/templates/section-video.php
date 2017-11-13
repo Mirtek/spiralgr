21ee1818ba5b5739ca350c70efefe18e5a247c22
Ievgenii Bondarenko <jenik_bond@ukr.net>
2017-11-13 15:19:23 +0200

    add section-video template (video link)

---
+++ wp-content/themes/spiralgroup/templates/section-video.php
@@ -0,0 +1,8 @@
+<section class="play-video-section">
+    <div class="content-wrapper clearfix">
+      <a class="video-link" href="<?php the_sub_field('vimeo_url') ?>">
+        <div class="backing"></div>
+        <i class="ico fa fa-play-circle-o" aria-hidden="true"></i>
+      </a>
+    </div>
+</section>
