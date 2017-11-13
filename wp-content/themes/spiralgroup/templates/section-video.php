bc1865f8fdff47cab00645074202fdbd65600270
Ievgenii Bondarenko <jenik_bond@ukr.net>
2017-11-13 15:38:20 +0200

    add background image in section-video

--- wp-content/themes/spiralgroup/templates/section-video.php
+++ wp-content/themes/spiralgroup/templates/section-video.php
@@ -1,6 +1,8 @@
+<?php $image = get_sub_field('image'); ?>
+
 <section class="play-video-section">
-    <div class="content-wrapper clearfix">
-      <a class="video-link" href="<?php the_sub_field('vimeo_url') ?>">
+    <div class="content-wrapper clearfix" style="background-image: url(<?php echo $image['url']; ?>);">
+      <a class="video-link" href="<?php the_sub_field('vimeo_url'); ?>">
         <div class="backing"></div>
         <i class="ico fa fa-play-circle-o" aria-hidden="true"></i>
       </a>
