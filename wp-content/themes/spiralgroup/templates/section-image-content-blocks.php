4956cb963143cf1a6635b8b790cdf688adc9177e
Denys Toloshny <gg.zyus@gmail.com>
2017-11-16 16:10:29 +0200

    add featured image_block style, fix some font responsive stuff

--- wp-content/themes/spiralgroup/templates/section-image-content-blocks.php
+++ wp-content/themes/spiralgroup/templates/section-image-content-blocks.php
@@ -68,14 +68,21 @@
                     case "featured":
                         $class="grid-item--height2 grid-item--width2";
                         $class_smaller="grid-smaller-item--height2 grid-smaller-item--width2";
+                        $hover_effect_class="grid-smaller-item--featured";
 
+                        if ( get_sub_field('hover_effect') == "secondary" ):
+                            $hover_effect_class="grid-smaller-item--featured2";
+                        endif;
                         ?>
                             
                         <div class="grid-item <?php echo $class ?>">
-                        <div class="grid-smaller-item <?php echo $class_smaller ?>">
+                        <div class="grid-smaller-item <?php echo $class_smaller ?> <?php echo $hover_effect_class ?>">
 
                             <div class="cards-title">
                               <p><?php echo get_sub_field('title') ?></p>
+                            </div>
+                            <div class="cards-subtitle">
+                              <p><?php echo get_sub_field('subtitle') ?></p>
                             </div>
                         </div>
                         </div>   
